b157a701faa147e0c1bf9ce100e38501f60b617f
Thiago Xavier <thiagox58@gmail.com>
2022-09-27 16:55:09 -0300

    Variáveis

---
+++ fundamentos/bloco-4-introducao-a-javascript/dia-1-javascript-primeiros-passos/exercicio-01/scripts.js
@@ -0,0 +1,13 @@
+const myName = 'Thiago Naifa';
+const birthCity = 'Amargosa';
+let birthYear = 1999;
+
+console.log('Meu nome é', myName + ',', 'nasci na cidade de', birthCity + ',', 'no ano de', birthYear+'.');
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+birthCity = 'São Paulo';
+
+console.log(birthCity);
